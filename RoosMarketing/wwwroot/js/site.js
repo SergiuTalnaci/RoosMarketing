75273a5ac859558ea5470cd9d7c9c47cf0d1a3ea
Sergiu Vlad <sergiu230@gmail.com>
2017-04-30 23:39:10 +0200

    Add project files.

---
+++ RoosMarketing/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Write your Javascript code.
+$("#two-three").click(function () {
+  $('.whiteboard-shrink').toggleClass('shrink');
+  $('#whiteboard-text').toggleClass('dissapear'); 
+});
+$("#three-four").click(function () {
+  $('.whiteboard-shrink').toggleClass('shrink');
+  $('#whiteboard-text').toggleClass('dissapear'); 
+});
+$("#three-two").click(function () {
+  $('.whiteboard-shrink').toggleClass('shrink');
+  $('#whiteboard-text').toggleClass('dissapear'); 
+});
+$("#four-three").click(function () {
+  $('.whiteboard-shrink').toggleClass('shrink');
+  $('#whiteboard-text').toggleClass('dissapear'); 
+});
